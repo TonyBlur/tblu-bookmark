84f21c61e8ecaaaf93dc8402d764e582ec1823cb
TonyBlur <zehao100@qq.com>
2024-09-20 00:38:50 +0800

    A test for environment variables on Vercel

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,11 @@
+const webpack = require('webpack');
 const Dotenv = require('dotenv-webpack');
 
 module.exports = {
   plugins: [
     new Dotenv(),
+    new webpack.DefinePlugin({
+      'process.env.BOOKMARK_DATA_URL': JSON.stringify(process.env.BOOKMARK_DATA_URL),
+    }),
   ],
 };

59eddc50ca82840902d17d0a5f824ca84f726b17
TonyBlu <zehao100@qq.com>
2025-02-13 23:35:05 +0800

    ✨ feat: Support realtime bookmark data refresh

---
+++ js/data-url-config.js
@@ -0,0 +1,19 @@
+const defaultBookmarkDataURL = 'json/pintree.json';
+let finalBookmarkDataURL = defaultBookmarkDataURL;
+
+try {
+  const response = await fetch('js/env-data.js');
+  const envData = await response.text();
+  eval(envData);
+  
+  if (window.env && window.env.BOOKMARK_DATA_URL) {
+    console.log('use BOOKMARK_DATA_URL:', window.env.BOOKMARK_DATA_URL);
+    finalBookmarkDataURL = window.env.BOOKMARK_DATA_URL;
+  } else {
+    console.log('use default BookmarkDataURL');
+  }
+} catch (error) {
+  console.log('Error loading env-data.js, using default BookmarkDataURL');
+}
+
+export const bookmarkDataURL = finalBookmarkDataURL;

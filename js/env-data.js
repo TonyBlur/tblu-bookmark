cdb3bc8f76524bd2e96c6b22ed4293b09dfd62fe
TonyBlu <101348912+TonyBlur@users.noreply.github.com>
2026-04-13 18:41:30 +0800

    fix: flatten search modal and derive alpha version from git

--- js/env-data.js
+++ js/env-data.js
@@ -1,4 +1,6 @@
 
 window.env = {
-  BOOKMARK_DATA_URL: ''
+  BOOKMARK_DATA_URL: '',
+  APP_PACKAGE_VERSION: '0.1.2',
+  COMMIT_SHORT: 'df969f4'
 };
--- js/generate-env.js
+++ js/generate-env.js
@@ -1,12 +1,22 @@
 const fs = require('fs');
 const path = require('path');
 const dotenv = require('dotenv');
+const { execSync } = require('child_process');
 
 dotenv.config();
+const pkg = require('../package.json');
+let commitShort = '';
+try {
+  commitShort = execSync('git rev-parse --short=7 HEAD', { encoding: 'utf8' }).trim();
+} catch (_) {
+  commitShort = '';
+}
 
 const envScriptContent = `
 window.env = {
-  BOOKMARK_DATA_URL: '${process.env.BOOKMARK_DATA_URL || ''}'
+  BOOKMARK_DATA_URL: '${process.env.BOOKMARK_DATA_URL || ''}',
+  APP_PACKAGE_VERSION: '${pkg.version || ''}',
+  COMMIT_SHORT: '${commitShort}'
 };
 `;
 
